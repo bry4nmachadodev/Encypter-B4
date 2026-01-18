458a3c61202a5722d86924deb079383dae3699b0
bry4nmachadodev <bryanmachado1703@icloud.com>
2026-01-17 22:34:54 -0300

    git innit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import encrypter.KeyManager;
+
+import javax.crypto.SecretKey;
+import java.security.NoSuchAlgorithmException;
+
+public class Main {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+
+    }
+}
