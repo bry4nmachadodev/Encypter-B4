2505115dc60c06280f67df8f1515bc25d87ab5c2
bry4nmachadodev <bryanmachado1703@icloud.com>
2026-01-19 12:05:57 -0300

    cli -> gui (FileEnciptador)

--- src/encrypter/FileEncryptor.java
+++ src/encrypter/FileEncryptor.java
@@ -4,29 +4,33 @@ import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileEncryptor {
 
     //Criptografar arquivo -> chave AES
     public void criptografarArquivo(SecretKey chave, Path arquivoOrigem) throws Exception {
-        //lê o arquivo recebido
         byte[] dados = Files.readAllBytes(arquivoOrigem);
         Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.ENCRYPT_MODE, chave);
         byte[] dadosCriptografado = cipher.doFinal(dados);
 
-        Files.write(arquivoOrigem, dadosCriptografado);
+        // Cria arquivo com extensão .encrypted
+        Path arquivoCriptografado = Paths.get(arquivoOrigem.toString() + ".encrypted");
+        Files.write(arquivoCriptografado, dadosCriptografado);
     }
 
     //Descriptografar arquivo -> chave AES
     public void descriptografarArquivo(SecretKey chave, Path arquivoOrigem) throws Exception {
-        //lê o arquivo recebido
         byte[] dados = Files.readAllBytes(arquivoOrigem);
         Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.DECRYPT_MODE, chave);
-        byte[] dadosCriptografado = cipher.doFinal(dados);
+        byte[] dadosDescriptografado = cipher.doFinal(dados);
 
-        Files.write(arquivoOrigem, dadosCriptografado);
+        // Remove o .encrypted do nome
+        String caminhoOriginal = arquivoOrigem.toString().replace(".encrypted", "");
+        Path arquivoDescriptografado = Paths.get(caminhoOriginal + ".decrypted");
+        Files.write(arquivoDescriptografado, dadosDescriptografado);
     }
 
 }
