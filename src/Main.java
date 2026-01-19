42a4c88e380522208cb8543d29730485b26a5cb6
bry4nmachadodev <bryanmachado1703@icloud.com>
2026-01-19 12:06:25 -0300

    cli -> gui (MAIN)

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
-import encrypter.Menu;
+import encrypter.EncriptadorGUI;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
 public class Main {
     public static void main(String[] args) {
-        Menu menu = new Menu();
-        menu.iniciar();
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        SwingUtilities.invokeLater(() -> {
+            EncriptadorGUI gui = new EncriptadorGUI();
+            gui.setVisible(true);
+        });
     }
 }
